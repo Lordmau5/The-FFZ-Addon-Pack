aa7b8ac30658d14907331a9861635765d3c9db0a
Lordmau5 <dustin.hecken@web.de>
2016-11-24 00:44:33 +0100

    Add a local server for debug stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,18 +4,18 @@ module.exports = function(grunt) {
     concat: {
       options: {
         // define a string to put between each file in the concatenated output
-        separator: ';'
+        separator: '\n/* --------------- */\n\n'
       },
       dist: {
         // the files to concatenate
-        src: ['src/main.js', 'src/**/*.js', 'src/init.js'],
+        src: ['src/main.js', 'src/**/*.js'],
         dest: 'dist/<%= pkg.name %>.js'
       }
     },
     uglify: {
       options: {
         // the banner is inserted at the top of the output
-        banner: '/*! <%= pkg.fullName %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+        banner: '/*! <%= pkg.fullName %> <%= grunt.template.today("dd-mm-yyyy HH:MM:ss") %> */\n'
       },
       dist: {
         files: {
@@ -40,6 +40,28 @@ module.exports = function(grunt) {
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint', 'concat', 'uglify']
+    },
+    connect: {
+      server: {
+        options: {
+          port: 3000,
+          hostname: '*',
+          base: 'dist',
+          keepalive: true,
+          protocol: 'https',
+          middleware: function(connect, options, middlewares) {
+            middlewares.unshift(function(req, res, next) {
+              res.setHeader('Access-Control-Allow-Origin', '*');
+              res.setHeader('Access-Control-Allow-Credentials', true);
+              res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+              res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+              next();
+            });
+
+            return middlewares;
+          }
+        }
+      }
     }
   });
 
@@ -47,8 +69,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
   grunt.registerTask('test', ['jshint']);
 
   grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+
+  grunt.registerTask('dev', ['jshint', 'concat', 'uglify', 'connect']);
 };
