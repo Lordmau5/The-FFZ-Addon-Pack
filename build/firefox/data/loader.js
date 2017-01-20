f4540b8924c631e100a31f2c337ab9058cc23b6e
Lordmau5 <dustin.hecken@web.de>
2017-01-20 22:36:09 +0100

    Remove Firefox files
    Will be replaced with the userscript because keeping up compatibility with FF is a pain.
    Thanks, Mozilla!

--- build/firefox/data/loader.js
+++
@@ -1,5 +0,0 @@
-var ce = document.createElement.bind(document);
-var s = ce("script");
-s.type = "text/javascript";
-s.src = "//cdn.lordmau5.com/ffz-ap/ffz-add-on-pack.min.js?_=" + Date.now();
-document.head.appendChild(s);
