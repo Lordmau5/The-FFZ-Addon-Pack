52cb1675615b3e57260be85488df77f8275bbd4b
Lordmau5 <dustin.hecken@web.de>
2017-01-24 19:21:46 +0100

    Update userscript to allow dev-server support

--- build/userscript/injector.user.js
+++ build/userscript/injector.user.js
@@ -30,8 +30,24 @@ function ffzap_init() {
 
     script.id = 'ffzap_script';
     script.type = 'text/javascript';
-    script.src = 'https://cdn.lordmau5.com/ffz-ap/ffz-ap.min.js';
+    if(localStorage.ffz_ap_debug_mode) {
+      var xhr = new XMLHttpRequest();
+  		xhr.open('GET', '//localhost:3000/', true);
+  		xhr.onload = function(e) {
+        console.log('FFZ:AP: Development Server is present.');
+  			script.src = '//localhost:3000/ffz-ap.js';
+  			document.body.classList.add('ffz-ap-dev');
+  			document.head.appendChild(script);
+      };
+      xhr.onerror = function(e) {
+        console.log('FFZ:AP: Development Server is not present. Using CDN.');
+  			script.src = '//lordmau5.com/nocache/ffz-ap/ffz-ap.min.js?_=' + Date.now();
+  			document.head.appendChild(script);
+      };
+      return xhr.send(null);
+    }
 
+    script.src = '//cdn.lordmau5.com/ffz-ap/ffz-ap.min.js';
     document.head.appendChild(script);
 }
 
