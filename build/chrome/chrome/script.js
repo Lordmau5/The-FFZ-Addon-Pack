9c6f366c85f79f24e55cf372e7d6b5e173094396
Lordmau5 <dustin.hecken@web.de>
2017-01-24 19:19:08 +0100

    Change link to load script from

--- build/chrome/chrome/script.js
+++ build/chrome/chrome/script.js
@@ -7,19 +7,19 @@ function inject() {
 		xhr.open('GET', '//localhost:3000/', true);
 		xhr.onload = function(e) {
       console.log('FFZ:AP: Development Server is present.');
-			script.src = '//localhost:3000/ffz-add-on-pack.js';
+			script.src = '//localhost:3000/ffz-ap.js';
 			document.body.classList.add('ffz-ap-dev');
 			document.head.appendChild(script);
     };
     xhr.onerror = function(e) {
       console.log('FFZ:AP: Development Server is not present. Using CDN.');
-			script.src = '//lordmau5.com/nocache/ffz-ap/ffz-add-on-pack.min.js?_=' + Date.now();
+			script.src = '//lordmau5.com/nocache/ffz-ap/ffz-ap.min.js?_=' + Date.now();
 			document.head.appendChild(script);
     };
     return xhr.send(null);
   }
 
-  script.src = '//cdn.lordmau5.com/ffz-ap/ffz-add-on-pack.min.js?_=' + Date.now();
+  script.src = '//cdn.lordmau5.com/ffz-ap/ffz-ap.min.js?_=' + Date.now();
 	document.head.appendChild(script);
 }
 
