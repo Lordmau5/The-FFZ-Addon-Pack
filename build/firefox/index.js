6e3bb8bc7f3fdd0717eaa36a4684f379f16e3c5d
Lordmau5 <dustin.hecken@web.de>
2017-02-05 20:02:33 +0100

    Less permissions required

--- build/firefox/index.js
+++ build/firefox/index.js
@@ -4,17 +4,3 @@ pageMod.PageMod({
 	include: "*.twitch.tv",
 	contentScriptFile: './loader.js'
 });
-
-browser.webRequest.onHeadersReceived.addListener(function(info) {
-    return {
-        responseHeaders: info.responseHeaders.concat([{
-            name: 'access-control-allow-origin',
-            value: '*'
-        }])
-    };
-}, {
-    urls: [
-        "*://cdn.betterttv.net/*",
-        "*://cdn.lordmau5.com/*"
-    ]
-}, ["blocking", "responseHeaders"]);
