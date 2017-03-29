59d204c282b9d0d194740f0b13aba003eca3cd16
Laura Lynn <Hatsuney@users.noreply.github.com>
2017-03-29 20:55:27 +0200

    your indentation is fucking triggering me

--- build/chrome/chrome/script.js
+++ build/chrome/chrome/script.js
@@ -1,26 +1,26 @@
 function inject() {
   var script = document.createElement('script');
   script.type = 'text/javascript';
-
+  
   if(localStorage.ffz_ap_debug_mode === 'true') {
     var xhr = new XMLHttpRequest();
-		xhr.open('GET', '//localhost:3000/', true);
-		xhr.onload = function(e) {
+    xhr.open('GET', '//localhost:3000/', true);
+    xhr.onload = function(e) {
       console.log('FFZ:AP: Development Server is present.');
-			script.src = '//localhost:3000/ffz-ap.js';
-			document.body.classList.add('ffz-ap-dev');
-			document.head.appendChild(script);
+      script.src = '//localhost:3000/ffz-ap.js';
+      document.body.classList.add('ffz-ap-dev');
+      document.head.appendChild(script);
     };
     xhr.onerror = function(e) {
       console.log('FFZ:AP: Development Server is not present. Using CDN.');
-			script.src = '//lordmau5.com/nocache/ffz-ap/ffz-ap.min.js?_=' + Date.now();
-			document.head.appendChild(script);
+      script.src = '//lordmau5.com/nocache/ffz-ap/ffz-ap.min.js?_=' + Date.now();
+      document.head.appendChild(script);
     };
     return xhr.send(null);
   }
 
   script.src = '//cdn.lordmau5.com/ffz-ap/ffz-ap.min.js';
-	document.head.appendChild(script);
+  document.head.appendChild(script);
 }
 
 inject();
