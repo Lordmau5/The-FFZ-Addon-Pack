47cb7ecb38b67dd8141adb5b22e1fb134177c056
Lordmau5 <mail@lordmau5.com>
2017-11-19 12:29:57 +0100

    Push last files
    "Kinda" 3.1?

--- build/firefox/data/loader.js
+++
@@ -1,5 +0,0 @@
-var ce = document.createElement.bind(document);
-var s = ce('script');
-s.type = 'text/javascript';
-s.src = 'https://cdn.ffzap.download/ffz-ap/ffz-ap.min.js';
-document.head.appendChild(s);
