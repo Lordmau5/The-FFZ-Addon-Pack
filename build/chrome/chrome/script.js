fc6f258b334b8fcfbb9bfe2769e5c4a317957bc5
Lordmau5 <mail@lordmau5.com>
2018-02-14 13:30:45 +0100

    Update Chrome script
    I should've added comments about the source code way, WAY sooner.
    
    Sorry, Google.

--- build/chrome/chrome/script.js
+++ build/chrome/chrome/script.js
@@ -1,5 +1,15 @@
-const ce = document.createElement.bind(document);
-const s = ce('script');
-s.type = 'text/javascript';
-s.src = 'https://cdn.ffzap.com/ffz-ap.min.js';
-document.head.appendChild(s);
+/*
+	All code is publicly available at:
+	https://github.com/Lordmau5/The-FFZ-Addon-Pack
+
+	This script is solely an injector for the actual script,
+	which is being appended into the document head on
+	*.twitch.tv.
+
+	No injection is made into the Chrome context.
+*/
+
+const script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://cdn.ffzap.com/ffz-ap.min.js';
+document.head.appendChild(script);
